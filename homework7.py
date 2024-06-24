6a48518e37e5e3bb5daaffe02d7c56a26ce0ecb5
KostyaFun <para-pa-pam11@mail.ru>
2024-06-24 23:29:40 +0300

    Add files via upload

---
+++ homework7.py
@@ -0,0 +1,9 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+Aaron_grade = ((5+3+3+5+4)/5)
+Bilbo_grade = ((2+2+2+3)/4)
+Johnny_grade = ((4+5+5+2)/4)
+Khendrik_grade = ((4+4+3)/3)
+Steve_grade = ((5+5+5+4+5)/5)
+students_grades = {'Aaron': Aaron_grade, 'Bilbo': Bilbo_grade, 'Johny': Johnny_grade, 'Khendrik': Khendrik_grade, 'Steve': Steve_grade}
+print(students_grades)
